4f795249a6de54b80d6bdf85c61c9e511c5fb3da
akhil2015 <akhilcool1996@gmail.com>
2017-07-01 17:37:04 +0530

    added topbar

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $(".navbar-default").css('background-color', '#333');
+          $(".nav-custom").css('margin-top',"0");
        } else {
           $('.navbar-default').css('background-color', 'transparent');
        }
