84b433d8b3197443c3bce81068d2c85e6081628e
Ravi Anand <ravianand.anand72@gmail.com>
2017-07-03 23:31:52 +0530

    book now button

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ $(document).ready(function(){
    });
     }
 });
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
